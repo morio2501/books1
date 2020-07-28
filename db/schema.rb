14293a408575836e83602509b744b469a5aadb32
vagrant <morio4899@gmail.com>
2020-07-28 02:37:53 +0000

    validate_last

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_121700) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "text"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
